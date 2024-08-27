e516248d6802dad099ee3c5cfb94fddab9583a52
youtlane <youtlanehanane@gmail.com>
2024-08-27 23:00:12 +0100

    fix: clean code

--- hrnet-react/src/app/allEmployee/page.jsx
+++ hrnet-react/src/app/allEmployee/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const allEmployee = () => {
+  // Use the useSelector hook to access the employees from the Redux store
+  const employees = useSelector((state) => state.employees);
+
+  console.log('employees ', employees);
   return (
-    <div>page</div>
-  )
+    <div>
+      <h1>Employee List { employees.length }</h1>
+      <ul>
+        {employees.length > 0 ? (
+          employees.map((employee, index) => (
+
+            
+            <li key={index}>{employee.firstName}</li>
+          ))
+        ) : (
+          <li>No employees available.</li>
+        )}
+      </ul>
+    </div>
+  );
 }
 
-export default page
+export default allEmployee;
