85a1e8837542921a3a14ba4e7d472fc4aee97f88
youtlane <youtlanehanane@gmail.com>
2024-09-16 12:05:56 +0200

    fix: disappearing of style while refreaching the page

--- hrnet-react/src/app/allEmployee/page.jsx
+++ hrnet-react/src/app/allEmployee/page.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { FaSearch, FaArrowLeft, FaArrowRight, FaSortUp, FaSortDown, FaHome } from "react-icons/fa";
 import Link from "next/link";
+import '../global.css';
 
 const AllEmployee = () => {
   const employees = useSelector((state) => state.employees);
